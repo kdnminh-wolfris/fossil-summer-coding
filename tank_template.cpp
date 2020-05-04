a9bd5922120b704cbfec326fe699348fa5c408b5
Le Duy Thuc <leduykhongngu@gmail.com>
2020-05-04 19:55:58 +0700

    Create checker teamplate, add documentation comments

--- tank_template.cpp
+++ tank_template.cpp
@@ -1,7 +1,15 @@
 #include <fstream>
+#include <iostream>
 using namespace std;
 class ThucSolve {
     public:
+        /**
+         * Read input 
+         * 
+         * @param inPath path to input file
+         * 
+         * Please remember to close the input stream
+         */ 
         void readInput(string inPath) {
             ifstream inp;
             inp.open(inPath);
@@ -10,6 +18,13 @@ class ThucSolve {
             //remember to close the input stream
             inp.close();
         }
+        /**
+         * Calculate the answer and write to output file 
+         * 
+         * @param outPath path to output file
+         * 
+         * Please remember to close the output stream
+         */ 
         void writeOutput(string outPath) {
             ofstream out;
             out.open(outPath);
@@ -20,9 +35,37 @@ class ThucSolve {
         }
 };
 
+
+class Checker {
+    public:
+        /**
+         * Verify output
+         * 
+         * @param inPath path to input file
+         * @param outPath path to output file
+         * @param comment Comment about the output
+         * @return a pair of number of stars and length of the path
+         * if answer is correct, otherwise it will return {-1, -1}
+         */
+        pair<int, int> verifyOutput(string inPath, string outPath, string &comment) {
+            //todo later;
+            return {-1, -1};
+        }
+};
 int main() {
     ThucSolve sol1;
     sol1.readInput("sample.inp");
     sol1.writeOutput("sample.out");
+    Checker checker;
+    string comment;
+    pair<int, int> result = checker.verifyOutput("sample.inp", "sample.out", comment);
+    if (result.first == -1) {
+        cout << comment;
+    }
+    else {
+        cout << "Correct answers\n";
+        cout << "Number of stars: " << result.first << '\n';
+        cout << "Path's length: " << result.second << '\n';
+    }
     return 0;
 }
