2c892b46577e9840acc8f343dea9b5b2a0a292df
Khau Dang Nhat Minh <khaudangnhatminh@gmail.com>
2020-05-08 11:48:43 +0700

    add checker

--- cpp.cpp
+++ cpp.cpp
@@ -5,6 +5,11 @@
 #include <vector>
 #include <queue>
 
+#include <stdarg.h>  // For va_start, etc.
+#include <string.h>
+#include <utility>
+#include <memory>  // For std::unique_ptr
+
 using namespace std;
 
 class Khau {
@@ -132,7 +137,7 @@ class Khau {
                 for (int xx, yy, gg, mm, i = 0; i < 4; ++i) {
                     xx = x + dirX[i];
                     yy = y + dirY[i];
-                    if (0 > xx || xx >= size_m || 0 > yy || yy >= size_n)
+                    if (0 > xx || xx >= size_m || 0 > yy || yy >= size_n || data[xx][yy] == 0)
                         continue;
 
                     if (data[xx][yy] == 2)
@@ -178,13 +183,141 @@ class Khau {
         void writeOutput(string outPath) {
             fo.open(outPath);
             vector <pair <int, int>> res = dijkstra();
-            for (auto p: res) fo << p.first + 1 << ' ' << p.second + 1 << '\n';
+            for (int i = 1; i < res.size(); ++i)
+                fo << res[i].first + 1 << ' ' << res[i].second + 1 << '\n';
             fo.close();
         }
 };
 
+#define WALL 0
+#define STREET 1
+#define GAS 2
+#define STAR 3
+#define EARNED_STAR 4
+
+class Checker {
+#define MAXN 1005
+   private:
+    int grid[MAXN][MAXN];
+    int nRow, nCol, tankX, tankY, gasMax;
+    /**
+     * format string like sprintf()
+     *
+     * Thanks to https://stackoverflow.com/a/8098080
+     */
+    string string_format(const string fmt_str, ...) {
+        int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+        unique_ptr<char[]> formatted;
+        va_list ap;
+        while (1) {
+            formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+            strcpy(&formatted[0], fmt_str.c_str());
+            va_start(ap, fmt_str);
+            final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+            va_end(ap);
+            if (final_n < 0 || final_n >= n)
+                n += abs(final_n - n + 1);
+            else
+                break;
+        }
+        return string(formatted.get());
+    }
+    /**
+     * Read input
+     *
+     * @param inPath path to input file
+     *
+     * Please remember to close the input stream
+     */
+    void readInput(string inPath) {
+        ifstream inp;
+        inp.open(inPath);
+        //put your code here
+        inp >> tankX >> tankY;
+        inp >> gasMax;
+        inp >> nRow >> nCol;
+        for (int i = 1; i <= nRow; ++i)
+            for (int j = 1; j <= nCol; ++j)
+                inp >> grid[i][j];
+        //remember to close the input stream
+        inp.close();
+    }
+
+   public:
+    /**
+     * Verify output
+     *
+     * @param inPath path to input file
+     * @param outPath path to output file
+     * @param comment Comment about the output
+     * @return a pair of number of stars and length of the path
+     * if answer is correct, otherwise it will return {-1, -1}
+     *
+     */
+    pair<int, int> verifyOutput(string inPath, string outPath, string &comment) {
+        readInput(inPath);
+        ifstream out;
+        out.open(outPath);
+#define verify(x, s)     \
+    if (!(x)) {          \
+        comment = s;     \
+        out.close();     \
+        return {-1, -1}; \
+    }
+        int nxtX, nxtY;
+        int len = 0;
+        int cntStar = 0;
+        int currentGas = gasMax;
+        int cntMapStar = 0;
+        for (int i = 1; i <= nRow; ++i)
+            for (int j = 1; j <= nCol; ++j)
+                cntMapStar += (grid[i][j] == STAR);
+        while (out >> nxtX >> nxtY) {
+            ++len;
+            verify(0 < nxtX && nxtX <= nRow,
+                   string_format("Move %d ((%d, %d) -> (%d, %d)) is invalid, expected `X` in range [%d, %d], but found %d",
+                                 len, tankX, tankY, nxtX, nxtY, 1, nRow, nxtX));
+            verify(0 < nxtY && nxtY <= nCol,
+                   string_format("Move %d ((%d, %d) -> (%d, %d)) is invalid, expected `Y` in range [%d, %d], but found %d",
+                                 len, tankX, tankY, nxtX, nxtY, 1, nCol, nxtY));
+            verify(abs(tankX - nxtX) + abs(tankY - nxtY) == 1,
+                   string_format("Move %d ((%d, %d) -> (%d, %d)) is invalid, expected manhattan's distance is 1, found %d",
+                                 len, tankX, tankY, nxtX, nxtY, abs(tankX - nxtX) + abs(tankY - nxtY)));
+            verify(grid[nxtX][nxtY] != WALL,
+                   string_format("Move %d ((%d, %d) -> (%d, %d)) is invalid, tank move into a forbiden cell",
+                                 len, tankX, tankY, nxtX, nxtY));
+            verify(currentGas != 0,
+                   string_format("Move %d ((%d, %d) -> (%d, %d)) is invalid, ran out of gas",
+                                 len, tankX, tankY, nxtX, nxtY));
+            --currentGas;
+            tankX = nxtX;
+            tankY = nxtY;
+            if (grid[tankX][tankY] == STAR)
+                cntStar += 1,
+                grid[tankX][tankY] = STREET;
+            if (grid[tankX][tankY] == GAS)
+                currentGas = gasMax;
+        }
+        comment = string_format("Map has %d stars", cntMapStar);
+        return {cntStar, len};
+    }
+#undef MAXN
+};
+
 int main() {
+/*
     Khau sol;
     sol.readInput("map.txt");
     sol.writeOutput("path.txt");
+*/
+
+    Checker check;
+    string cmt;
+    pair <int, int> ans = check.verifyOutput("map.txt", "path.txt", cmt);
+    if (ans.first == -1)
+        cout << cmt;
+    else {
+        cout << "Stars collected: " << ans.first << '\n';
+        cout << "Path length: " << ans.second;
+    }
 }
